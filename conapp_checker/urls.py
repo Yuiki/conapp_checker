7dd4b8604ce12a1c6b11915dfba85b7d60500e8f
Yuiki <contact@yuiki.jp>
2017-11-18 10:08:07 +0900

    initial commit

---
+++ conapp_checker/urls.py
@@ -0,0 +1,32 @@
+"""conapp_checker URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+import django.contrib.auth.views
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from conapp_checker import views
+
+urlpatterns = [
+    url(r'^check/$', views.check, name='check'),
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('social_django.urls', namespace='social')),
+    url(r'^$',
+        django.contrib.auth.views.login,
+        {
+            'template_name': 'app/index.html'
+        },
+        name='login'),
+]
---
+++ conapp_checker/views.py
@@ -0,0 +1,56 @@
+from datetime import datetime, timedelta
+
+import tweepy
+from django.conf import settings
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+from social_django.models import UserSocialAuth
+
+
+@login_required
+def check(request):
+    user = UserSocialAuth.objects.get(user_id=request.user.id)
+    oauth_token = user.extra_data['access_token']['oauth_token']
+    oauth_token_secret = user.extra_data['access_token']['oauth_token_secret']
+    logout(request)
+    consumer_key = settings.SOCIAL_AUTH_TWITTER_KEY
+    consumer_secret = settings.SOCIAL_AUTH_TWITTER_SECRET
+    summary = calc_tweet_summary(consumer_key, consumer_secret, oauth_token, oauth_token_secret)
+    return render(request, 'app/check.html', dict(summary=summary))
+
+
+def calc_tweet_summary(consumer_key, consumer_secret, access_token, access_token_secret):
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+
+    hashtag = '#コンテンツ応用論2017'
+    date_fmt = '%m/%d'
+
+    lecture_dates = [(10, 2), (10, 10), (10, 16), (10, 23), (10, 30), (11, 13)]
+    lecture_start_times = [datetime(2017, date[0], date[1], 6) for date in lecture_dates]
+    summary = {date.strftime(date_fmt): 0 for date in lecture_start_times}
+
+    max_id = api.user_timeline(count=1)[0].id
+
+    cont = True
+    while cont:
+        try:
+            statuses = api.user_timeline(count=200, max_id=max_id)
+        except tweepy.RateLimitError:
+            break
+        if not statuses:
+            break
+        for status in statuses:
+            max_id = status.id - 1
+            first_lecture_date = lecture_start_times[0]
+            tweet_time = status.created_at
+            if tweet_time < first_lecture_date:
+                cont = False
+            if hashtag in status.text:
+                for lecture_start_time in lecture_start_times:
+                    dead_line = lecture_start_time + timedelta(weeks=1)
+                    if lecture_start_time <= tweet_time < dead_line:
+                        summary[lecture_start_time.strftime(date_fmt)] += 1
+    return summary
