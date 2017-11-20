733b25f62af47f60cb4af6bedf1b5f447635cc19
Yuiki <contact@yuiki.jp>
2017-11-20 22:53:29 +0900

    :recycle: resolve #8

--- conapp_checker/views.py
+++ conapp_checker/views.py
@@ -8,6 +8,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from social_django.models import UserSocialAuth
 
+DATE_FMT = '%m/%d'
+
 
 @login_required
 def check(request):
@@ -17,56 +19,84 @@ def check(request):
     logout(request)
     consumer_key = settings.SOCIAL_AUTH_TWITTER_KEY
     consumer_secret = settings.SOCIAL_AUTH_TWITTER_SECRET
-    summary = calc_tweet_summary(consumer_key, consumer_secret, oauth_token, oauth_token_secret)
+    summary = summarize_conapp_statuses(consumer_key, consumer_secret, oauth_token, oauth_token_secret)
     return render(request, 'app/check.html', dict(summary=summary))
 
 
-def calc_tweet_summary(consumer_key, consumer_secret, access_token, access_token_secret):
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
-
+def summarize_conapp_statuses(consumer_key, consumer_secret, access_token, access_token_secret):
+    status_fetcher = MyStatusFetcher(consumer_key, consumer_secret, access_token, access_token_secret)
     hashtag = '#コンテンツ応用論2017'
-    date_fmt = '%m/%d'
-
+    # 第一回は集計対象外
     lecture_dates = [(10, 2), (10, 10), (10, 16), (10, 23), (10, 30), (11, 13), (11, 20)]
-    lecture_date_times = [datetime(2017, date[0], date[1]) for date in lecture_dates]
-    summary = OrderedDict([(date.strftime(date_fmt), 0) for date in lecture_date_times])
+    periods = []
+    for idx, date in enumerate(lecture_dates):
+        lecture_date = datetime(2017, date[0], date[1])
+        name = lecture_date.strftime(DATE_FMT)
+        end = lecture_date + timedelta(days=1)
+        if idx == 0:
+            start = end
+        else:
+            previous_period = periods[idx - 1]
+            start = previous_period.end
+        periods.append(Period(name, start, end))
+    else:
+        del periods[0]
 
-    max_id = -1
+    summarizer = StatusSummarizer(status_fetcher)
+    return summarizer.summarize(hashtag, periods)
 
-    cont = True
-    while cont:
-        try:
-            statuses = get_user_timeline(api, max_id)
-        except tweepy.RateLimitError:
-            break
-        if not statuses:
-            break
-        for status in statuses:
-            calc_start = lecture_date_times[0] + timedelta(days=1)
+
+class StatusSummarizer:
+    def __init__(self, status_fetcher):
+        self.fetcher = status_fetcher
+
+    def summarize(self, query, periods):
+        summary = OrderedDict([(period.name, 0) for period in periods])
+        calc_start = periods[0].start
+        for status in self.fetcher.gen_status():
             tweet_time = status.created_at
             if tweet_time < calc_start:
-                cont = False
-            if hashtag in status.text:
-                for idx, lecture_date in enumerate(lecture_date_times):
-                    # 第一回目は集計対象外
-                    if idx == 0:
-                        continue
-                    previous_lecture_date = lecture_date_times[idx - 1]
-                    start = previous_lecture_date + timedelta(days=1)
-                    end = lecture_date + timedelta(days=1)
-                    if start <= tweet_time < end:
-                        summary[lecture_date.strftime(date_fmt)] += 1
-        else:
-            max_id = statuses[-1].id - 1
-    # 第一回は集計対象外
-    summary.popitem(last=False)
-    return summary
+                return summary
+            if query in status.text:
+                summary[Period.find(periods, tweet_time)] += 1
+        return summary
 
 
-def get_user_timeline(api, max_id):
-    count = 200
-    if max_id == -1:
-        return api.user_timeline(count=count)
-    return api.user_timeline(count=count, max_id=max_id)
+class Period:
+    def __init__(self, name, start, end):
+        self.name = name
+        self.start = start
+        self.end = end
+
+    @staticmethod
+    def find(periods, target):
+        for period in periods:
+            if period.start <= target < period.end:
+                return period.name
+        return None
+
+
+class MyStatusFetcher:
+    COUNT = 200
+
+    def __init__(self, consumer_key, consumer_secret, access_token, access_token_secret):
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        self.api = tweepy.API(auth)
+
+    def gen_status(self):
+        max_id = None
+        while True:
+            try:
+                if not max_id:
+                    statuses = self.api.user_timeline(count=self.COUNT)
+                else:
+                    statuses = self.api.user_timeline(count=self.COUNT, max_id=max_id)
+            except tweepy.RateLimitError:
+                return
+            if statuses:
+                max_id = statuses[-1].id - 1
+            else:
+                return
+            for status in statuses:
+                yield status
